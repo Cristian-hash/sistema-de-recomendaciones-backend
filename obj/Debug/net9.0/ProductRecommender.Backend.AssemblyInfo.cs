437f80533860d92344a0edde9ada96f776b5a648
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-01-31 15:29:58 -0500

    Optimizacion de las consultas

--- obj/Debug/net9.0/ProductRecommender.Backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProductRecommender.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductRecommender.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91dd4f8808e568e492040c2498f7e4d8557db02d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e0ea90d91940ba5a4aa359020039d38828ce027")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductRecommender.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductRecommender.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
