3854e1157368b4255a02f9910ea1c0d90140b9a3
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-02-11 12:22:17 -0500

    escritura automatica

--- obj/Debug/net9.0/ProductRecommender.Backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProductRecommender.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductRecommender.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c1ab5db8c234990f818dbfc0d7a4f4ac17f2c64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce409eb5aedaa9d316a2cfb9a8bcec5febf88dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductRecommender.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductRecommender.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
